9d590380fb1a75f026829fcd275ba451c454c722
NektoUa <illyabarkov@gmail.com>
2021-12-16 17:40:15 +0200

    a little change with Bulka

--- javascript/main.js
+++ javascript/main.js
@@ -1,21 +1,21 @@
 const hello = () => {
     let hourNow = new Date().getHours();
-    let greeting = '';
-    if (hourNow > 18) greeting = 'Hello!';
-    else if (hourNow > 12) greeting = 'Hola!';
-    else if (hourNow > 0) greeting = 'Bonjour!';
-    else greeting = 'Вітаю!';
+    let greeting = "";
+    if (hourNow > 18) greeting = "Hello!";
+    else if (hourNow > 12) greeting = "Hola!";
+    else if (hourNow > 0) greeting = "Bonjour!";
+    else greeting = "Вітаю!";
     return greeting
 }
-document.querySelector('#part-day').innerHTML = hello();
+document.querySelector("#part-day").innerHTML = hello();
 
 
-let socialWrapper = document.querySelector('.socialWrapper')
+let socialWrapper = document.querySelector(".socialWrapper")
 
 function mouseIn(value) {
     let targetId = value.srcElement.id;
     for (let i = 1; i <= 4; i++) {
-        if (socialWrapper.classList.contains('socialActive' + i)) socialWrapper.classList.remove("socialActive" + i);
+        if (socialWrapper.classList.contains("socialActive" + i)) socialWrapper.classList.remove("socialActive" + i);
     }
     socialWrapper.classList.toggle("socialActive" + targetId);
 }
@@ -25,11 +25,11 @@ function mouseOut(value) {
     socialWrapper.classList.remove("socialActive" + targetId);
 }
 
-document.querySelectorAll('.social li a').forEach((item, i) => {
-    item.addEventListener('mouseover', mouseIn)
+document.querySelectorAll(".social li a").forEach((item, i) => {
+    item.addEventListener("mouseover", mouseIn)
 })
-document.querySelectorAll('.social li a').forEach((item, i) => {
-    item.addEventListener('mouseout', mouseOut)
+document.querySelectorAll(".social li a").forEach((item, i) => {
+    item.addEventListener("mouseout", mouseOut)
 })
 
 
