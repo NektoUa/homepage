3aedbbf26efb870a1865e9482d32773e9c55e8f1
NektoUa <illyabarkov@gmail.com>
2021-12-16 15:57:48 +0200

    add my photo and greeting

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,15 @@
+const hello = () => {
+    let hourNow = new Date().getHours();
+    let greeting = '';
+    if (hourNow > 18) greeting = 'Hello!';
+    else if (hourNow > 12) greeting = 'Hola!';
+    else if (hourNow > 0) greeting = 'Bonjour!';
+    else greeting = 'Вітаю!';
+    return greeting
+}
+document.querySelector('#part-day').innerHTML = hello();
+
+
 let socialWrapper = document.querySelector('.socialWrapper')
 
 function mouseIn(value) {
